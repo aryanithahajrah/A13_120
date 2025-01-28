6469c4dc7d72a6ec6799eee2f58d88546e542fce
aryanithahajrahmawaddah <aryanithahajrah@gmail.com>
2025-01-28 23:41:51 +0700

    finish

---
+++ app/src/main/java/com/example/finalproject/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.finalproject
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.finalproject.ui.PenjualanApp
+import com.example.finalproject.ui.theme.FINALPROJECTTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            FINALPROJECTTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    PenjualanApp(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    FINALPROJECTTheme {
+        Greeting("Android")
+    }
+}
